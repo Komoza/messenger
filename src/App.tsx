e52f5d3cc8199b542fd06109658e612885393e1f
Maxim <MaximKomoza@gmail.com>
2024-05-22 22:15:15 +0700

    generate names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import {useEffect, useState} from "react";
+import {NAMES} from "./tmp/data-names.ts";
+
 function App() {
+  const [person, setPerson] = useState<string>('World')
+
+  useEffect(() =>{
+    const intervalIndex = setInterval(() => {
+      const idx = Math.floor(Math.random() * NAMES.length);
+      setPerson(NAMES[idx])
+    }, 3000)
+
+    return () => clearInterval(intervalIndex)
+  }, [])
+
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, {person}!</h1>
     </div>
   )
 }
